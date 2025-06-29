b7984d8e2e4279eea236696c626638d5e32f8b51
catmaker <rzt0317@nate.com>
2025-06-29 17:39:40 +0900

    chore: nest.js prisma 연동

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('posts')
+  async getPosts() {
+    return this.prisma.post.findMany();
   }
 }
